d1ebd0adccfe81de1fc19742c7aeb61503608060
Rui Marinho <me@ruimarinho.net>
2014-08-08 02:09:28 +0100

    update nugets

--- XF.Labs.Sample/XF.Labs.Sample/XF.Labs.Sample/Pages/Mvvm/CanvasWebHybrid.xaml.cs
+++ XF.Labs.Sample/XF.Labs.Sample/XF.Labs.Sample/Pages/Mvvm/CanvasWebHybrid.xaml.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using Xamarin.Forms;
 using Xamarin.Forms.Labs.Mvvm;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
 
 namespace XF.Labs.Sample
-{	
+{
     public partial class CanvasWebHybrid : BaseView
-	{	
-		public CanvasWebHybrid ()
-		{
-			InitializeComponent ();
+    {
+        public CanvasWebHybrid()
+        {
+            InitializeComponent();
 
             this.hybridWebView.RegisterCallback("dataCallback", t =>
                 System.Diagnostics.Debug.WriteLine(t)
@@ -30,32 +33,36 @@ namespace XF.Labs.Sample
                 datapoint.PropertyChanged += HandlePropertyChanged;
             }
 
-            this.hybridWebView.Uri = new Uri ("https://raw.githubusercontent.com/sami1971/SimplyMobile/master/iOS/Tests/WebClientTests/Content/home.html");
-		}
+            this.hybridWebView.LoadFinished += (s, e) =>
+            {
+                this.hybridWebView.CallJsFunction("onViewModelData", this.BindingContext);
+            };
+        }
 
-        void HandleCollectionChanged (object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        void HandleCollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
         {
-//            if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Add)
-//            {
-                foreach (var datapoint in e.NewItems.OfType<DataPoint>())
-                {
-                    datapoint.PropertyChanged += HandlePropertyChanged;
-                }
-//            } 
-//            else if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Remove)
-//            {
-                foreach (var datapoint in e.OldItems.OfType<DataPoint>())
-                {
-                    datapoint.PropertyChanged -= HandlePropertyChanged;
-                }
-//            }
-            
+            foreach (var datapoint in e.NewItems.OfType<DataPoint>())
+            {
+                datapoint.PropertyChanged += HandlePropertyChanged;
+            }
+
+            foreach (var datapoint in e.OldItems.OfType<DataPoint>())
+            {
+                datapoint.PropertyChanged -= HandlePropertyChanged;
+            }
         }
 
         void HandlePropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
         {
-            this.hybridWebView.CallJsFunction ("onViewModelData", this.BindingContext);
+            this.hybridWebView.CallJsFunction("onViewModelData", this.BindingContext);
         }
-	}
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            this.hybridWebView.LoadFromContent("HTML/home.html");
+        }
+    }
 }
 
